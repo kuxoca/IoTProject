e51b00b19e0fad35b31ac1e815523dc2baccda6c
kuxoca <kuxoca@gmail.com>
2023-07-23 19:00:44 +0300

    recognize работает в базовой версии 2

--- module1/src/main/java/ppzeff/recognize/sber/RecognizeService.java
+++ module1/src/main/java/ppzeff/recognize/sber/RecognizeService.java
@@ -5,9 +5,6 @@ import java.util.function.Consumer;
 public interface RecognizeService {
     void recognize(byte[] bytes, Consumer<String> callback) ;
 
-//    void recognize(byte[] bytes, String lang);
-//    void recognize(byte[] bytes);
-
     void recognize(byte[] bytes, String lang, Consumer<String> callback) throws InterruptedException;
     void shutdown() throws InterruptedException;
 }
